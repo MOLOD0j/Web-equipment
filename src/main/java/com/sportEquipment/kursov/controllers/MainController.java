f96695d5fb8b940a7ddf818aec26d8bc248ec42a
Dmitry Vasilev <vasilevdimka@mail.ru>
2021-05-26 13:20:20 +0400

    Add login and registration

--- src/main/java/com/sportEquipment/kursov/controllers/MainController.java
+++ src/main/java/com/sportEquipment/kursov/controllers/MainController.java
@@ -3,6 +3,9 @@ package com.sportEquipment.kursov.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -30,4 +33,24 @@ public class MainController {
         return "FAQ";
     }
 
+    @RequestMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("title", "Регистрация");
+        return "registration";
+    }
+
+    @RequestMapping("/log_in")
+    public String log_in(Model model) {
+        model.addAttribute("title", "Логин");
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "log_in";
+    }
+
+    @RequestMapping("registration_add_user")
+    public String assortmentPostAdd(@RequestParam String name, @RequestParam String email,
+                                    @RequestParam String login, @RequestParam String password, Model model){
+        User user = new User(name, email, login, password);
+        userRepository.save(user);
+        return "redirect:/assortment";
+    }
 }
